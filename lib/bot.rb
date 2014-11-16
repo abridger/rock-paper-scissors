31cbbe3134e30c2519f525a37e72db810f3c66e0
Alan Bridger <alanrbridger@gmail.com>
2014-11-16 17:39:28 +0000

    Adds computer player class, a sub-class of player.

---
+++ lib/bot.rb
@@ -0,0 +1,19 @@
+require 'player'
+
+class Bot < Player
+
+  attr_reader :selection
+
+  DEFAULT_SET = ["Rock", "Paper", "Scissors"]
+
+  def initialize(name, object_set = DEFAULT_SET)
+    @name = name
+    @points = 0
+    @object_set = DEFAULT_SET
+  end
+
+  def select_object
+    @selection=(@object_set.sample)
+  end
+
+end
