ef23fac4f23be6106e847aac08a7cbb7de851266
Alan Bridger <alanrbridger@gmail.com>
2014-11-23 15:17:35 +0000

    Updating tests to match revised workflow.

---
+++ features/2 playing_the_game.feature
@@ -0,0 +1,9 @@
+Feature: Playing the game
+  In order to play Rock, Paper, Scissors
+  As a registered player
+  I want to be able choose an object
+
+  Scenario: Choosing an object to play
+    Given I have registered
+    And I click the button "Rock"
+    Then I should see the message "Round 2"
--- features/step_definitions/starting_a_game_steps.rb
+++ features/step_definitions/starting_a_game_steps.rb
@@ -1,21 +1,33 @@
 Given(/^I am on the homepage$/) do
-  visit '/'
+    visit '/'
 end
 
 Then(/^I should see the message "(.*?)"$/) do |message|
-  expect(page).to have_content(message)
+    expect(page).to have_content(message)
 end
 
 Given(/^I enter a username$/) do
-  fill_in('username', :with => 'Badger')
+    fill_in('username', :with => 'Badger')
 end
 
-Given(/^I click "(.*?)"$/) do |button|
-  click_button(button)
+Given(/^I click the button "(.*?)"$/) do |button|
+    click_button(button)
+end
+
+Given(/^I click the link "(.*?)"$/) do |link_text|
+  click_link(link_text)
 end
 
 Given(/^I have registered$/) do
-  step('I am on the homepage')
-  step('I enter a username')
-  step('I click "Register"')
+    visit '/reset'
+    visit '/'
+    fill_in('username', :with => 'Badger')
+    click_button('Register')
+end
+
+def in_browser(name)
+  old_session = Capybara.session_name
+  Capybara.session_name = name
+  yield
+  Capybara.session_name = old_session
 end
